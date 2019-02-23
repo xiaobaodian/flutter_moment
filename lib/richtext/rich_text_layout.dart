66295b0583819045e1e4a00ca4a5082e07de062e
zhangji <zhangji1969@163.com>
2019-02-23 12:53:14 +0800

    继续搭建richtext功能

--- lib/richtext/rich_text_layout.dart
+++ lib/richtext/rich_text_layout.dart
@@ -1,75 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_moment/richtext/cat_richtext.dart';
 
 class RichTextLayout {
-  RichTextLayout({
+  RichTextLayout(
+    BuildContext context, {
     this.segmentSpacing = 12.0,
     this.listLineSpacing = 3.0,
     this.leadingSymbols = '•',
     this.titleStyle,
-  });
+    this.subTitleStyle,
+    this.contentStyle,
+    this.contentBoldStyle,
+    this.taskStyle,
+    this.orderedListsStyle,
+    this.unorderedListStyle,
+    this.referenceStyle,
+    this.imageTextStyle,
+  }) : defaultTextStyle = DefaultTextStyle.of(context);
 
   final double segmentSpacing;
   final double listLineSpacing;
   final String leadingSymbols;
+  final DefaultTextStyle defaultTextStyle;
   final TextStyle titleStyle;
+  final TextStyle subTitleStyle;
+  final TextStyle contentStyle;
+  final TextStyle contentBoldStyle;
+  final TextStyle taskStyle;
+  final TextStyle orderedListsStyle;
+  final TextStyle unorderedListStyle;
+  final TextStyle referenceStyle;
+  final TextStyle imageTextStyle;
+
+  /// 基础布局
 
   Widget richLayoutText(Widget widget) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: widget,
-    );
+    return widget;
   }
 
   Widget richLayoutTask(Widget widget) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              width: 60,
-              height: double.infinity,
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(3, 3, 12, 0),
               child: Checkbox(
                 value: false,
-                onChanged: (isSelected){
-
-                },
+                onChanged: (isSelected) {},
               ),
-            ),
-            widget,
-          ]
-      ),
-    );
+          ),
+          widget,
+        ]);
   }
 
-  Widget richLayoutList(Widget leading, Widget widget) {  // Reference
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(3, 3, 12, 0),
-              child: leading,
-            ),
-            widget,
-          ]
-      ),
+  Widget richLayoutList(Widget leading, Widget widget) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(3, 3, 12, 0),
+            child: leading,
+          ),
+          Expanded(
+            child: widget,
+          ),
+        ]
     );
   }
 
   Widget richLayoutReference(Widget widget) {
     return Container(
       child: widget,
-      margin: EdgeInsets.fromLTRB(16, 0, 12, 0),
       padding: EdgeInsets.fromLTRB(16, 12, 16, 12),
       decoration: BoxDecoration(
         color: Colors.black12,
-        border: Border(
-            left: BorderSide(color: Colors.black26, width: 3.0)
-        ),
+        border: Border(left: BorderSide(color: Colors.black26, width: 3.0)),
       ),
     );
   }
@@ -80,4 +87,122 @@ class RichTextLayout {
       child: Placeholder(),
     );
   }
+
+  /// 分类布局
+
+  Widget getTitleLayout(String title) {
+    var effectiveSytle = titleStyle == null
+        ? TextStyle(fontSize: 18)
+        : titleStyle;
+    return richLayoutText(Text(
+      title,
+      style: effectiveSytle,
+    ));
+  }
+
+  Widget getSubTitleLayout(String subtitle) {
+    var effectiveSytle = subTitleStyle == null
+        ? TextStyle(fontSize: 16)
+        : subTitleStyle;
+    return richLayoutText(Text(
+      subtitle,
+      style: effectiveSytle,
+    ));
+  }
+
+  Widget getContentLayout(String text) {
+    var effectiveSytle = contentStyle == null
+        ? TextStyle(fontSize: 14)
+        : contentStyle;
+    return richLayoutText(Text(
+      text,
+      style: effectiveSytle,
+    ));
+  }
+
+  Widget getContentBoldLayout(String text) {
+    var effectiveSytle = contentBoldStyle == null
+        ? TextStyle(fontSize: 14, fontWeight: FontWeight.bold)
+        : contentBoldStyle;
+    return richLayoutText(Text(
+      text,
+      style: effectiveSytle,
+    ));
+  }
+
+  Widget getTaskLayout(String text) {
+    var effectiveSytle = taskStyle == null
+        ? TextStyle(fontSize: 12)
+        : taskStyle;
+    return richLayoutTask(Text(text, style: effectiveSytle));
+  }
+
+  Widget getOrderedListsLayout(String leading, String text) {
+    var effectiveSytle = orderedListsStyle == null
+        ? TextStyle(fontSize: 12) //defaultTextStyle.style.merge(TextStyle(fontSize: 12))
+        : orderedListsStyle;
+    return richLayoutList(Text('$leading.', style: effectiveSytle),
+        Text(text, style: effectiveSytle, softWrap: true));
+  }
+
+  Widget getUnorderedListLayout(String text) {
+    var effectiveSytle = unorderedListStyle == null
+        ? TextStyle(fontSize: 12)
+        : unorderedListStyle;
+    return richLayoutList(Text(leadingSymbols, style: effectiveSytle),
+        Text(text, style: effectiveSytle));
+  }
+
+  Widget getReferenceLayout(String text) {
+    var effectiveSytle = referenceStyle == null
+        ? TextStyle(fontSize: 12)
+        : referenceStyle;
+    return richLayoutReference(Text(text, style: effectiveSytle));
+  }
+
+  Widget getImageLayout(String image) {
+    return richLayoutImage(
+      Text(
+        '这里是图片',
+        style: TextStyle(fontSize: 12),
+      ),
+    );
+  }
+
+  ///
+
+  Widget getLayoutWidget(RichTextLine item){
+    Widget lineWidget;
+    switch (item.type) {
+      case RichLineType.Title :
+        lineWidget = getTitleLayout(item.content);
+        break;
+      case RichLineType.SubTitle :
+        lineWidget = getSubTitleLayout(item.content);
+        break;
+      case RichLineType.Text :
+        lineWidget = getContentLayout(item.content);
+        break;
+      case RichLineType.TextBold :
+        lineWidget = getContentBoldLayout(item.content);
+        break;
+      case RichLineType.Task :
+        lineWidget = getTaskLayout(item.content);
+        break;
+      case RichLineType.OrderedLists :
+        lineWidget = getOrderedListsLayout(item.leading, item.content);
+        break;
+      case RichLineType.UnorderedList :
+        lineWidget = getUnorderedListLayout(item.content);
+        break;
+      case RichLineType.Reference :
+        lineWidget = getReferenceLayout(item.content);
+        break;
+      case RichLineType.Image :
+        lineWidget = getImageLayout(item.content);
+        break;
+    }
+    return lineWidget;
+  }
+
 }
--- lib/route/rich_text_editer.dart
+++ lib/route/rich_text_editer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_moment/global_store.dart';
 import 'package:flutter_moment/richtext/cat_richtext.dart';
+import 'package:flutter_moment/richtext/rich_text_layout.dart';
 
 class RichTextEditerRoute extends StatefulWidget {
   final String _focusEvent;
@@ -28,11 +29,11 @@ class RichTextEditerRouteState extends State<RichTextEditerRoute> {
     richTextLine.add(RichTextLine(type: RichLineType.SubTitle, content: '这是副标题'));
     richTextLine.add(RichTextLine(type: RichLineType.Text, content: '循礼门小龙坎火锅，这里是位置的地址'));
     richTextLine.add(RichTextLine(type: RichLineType.UnorderedList, content: '这里是无序列表'));
-    richTextLine.add(RichTextLine(type: RichLineType.UnorderedList, content: '这里是无序列表'));
+    richTextLine.add(RichTextLine(type: RichLineType.UnorderedList, content: '这里是无序列表，这里是无序列表，这里是无序列表，这里是无序列表。'));
     richTextLine.add(RichTextLine(type: RichLineType.UnorderedList, content: '这里是无序列表'));
     richTextLine.add(RichTextLine(type: RichLineType.Text, content: '这里是普通的文本，用于对事物的描述。这里是普通的文本，用于对事物的描述。这里是普通的文本，用于对事物的描述。这里是普通的文本，用于对事物的描述。'));
     richTextLine.add(RichTextLine(type: RichLineType.OrderedLists, content: '这里是有序列表'));
-    richTextLine.add(RichTextLine(type: RichLineType.OrderedLists, content: '这里是有序列表'));
+    richTextLine.add(RichTextLine(type: RichLineType.OrderedLists, content: '这里是有序列表，这里是有序列表，这里是有序列表。这里是有序列表，这里是有序列表，这里是有序列表。'));
     richTextLine.add(RichTextLine(type: RichLineType.OrderedLists, content: '这里是有序列表'));
     richTextLine.add(RichTextLine(type: RichLineType.Reference, content: '这里是引用或需要特别说明的文本。这里是引用或需要特别说明的文本。这里是引用或需要特别说明的文本。'));
   }
@@ -60,26 +61,33 @@ class RichTextEditerRouteState extends State<RichTextEditerRoute> {
 
   @override
   Widget build(BuildContext context) {
+    var layout = RichTextLayout(context,
+      titleStyle: TextStyle(fontSize: 20, color: Colors.red),
+    );
     richOrderedList();
     return Scaffold(
       appBar: AppBar(
         title: Text('RichTextEditer'),
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) {
-          return getRichLineWidget(richTextLine[index]);
-        },
-        separatorBuilder: (context, index) {
-          if (index == richTextLine.length) {
-            return SizedBox(height: 12.0);
-          } else if (richTextLine[index].type == RichLineType.OrderedLists || richTextLine[index].type == RichLineType.UnorderedList) {
-            if (richTextLine[index+1].type == RichLineType.OrderedLists || richTextLine[index+1].type == RichLineType.UnorderedList) {
-              return SizedBox(height: 3.0, width: double.infinity,);
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            //return getRichLineWidget(richTextLine[index]);
+            return layout.getLayoutWidget(richTextLine[index]);
+          },
+          separatorBuilder: (context, index) {
+            if (index == richTextLine.length) {
+              return SizedBox(height: 12.0);
+            } else if (richTextLine[index].type == RichLineType.OrderedLists || richTextLine[index].type == RichLineType.UnorderedList) {
+              if (richTextLine[index+1].type == RichLineType.OrderedLists || richTextLine[index+1].type == RichLineType.UnorderedList) {
+                return SizedBox(height: 3.0, width: double.infinity,);
+              }
             }
-          }
-          return SizedBox(height: 12.0);
-        },
-        itemCount: richTextLine.length),
+            return SizedBox(height: 12.0);
+          },
+          itemCount: richTextLine.length),
+      ),
     );
   }
 }
