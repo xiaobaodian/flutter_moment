07c85511c0f5e2bb550aa74271cae4631bd72b8d
zhangji <zhangji1969@163.com>
2019-03-26 11:38:01 +0800

    新建image库，加入位置、人物、标签的引用和自动修改 完善了“位置”数据的分类显示和自动修改 改正了一些bug，优化了一些代码。

--- lib/models/data_services.dart
+++ lib/models/data_services.dart
@@ -1,34 +1,62 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'package:flutter_moment/models/models.dart';
-
-class FocusDateServices {
-
-  static List<PlaceItem> getPlaceList() {
-    List<PlaceItem> list = List<PlaceItem>();
-    list.add(PlaceItem(title: '循礼门小龙坎火锅', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '汉街万达广场', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '汉街小米之家旗舰店', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '群星城', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '1818广场', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '迈欧中冶店', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '汉街万达电影城', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '徐源记烧烤', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '中南商场沸腾鱼', address: '这里是位置的地址'));
-    list.add(PlaceItem(title: '汉街', address: '这里是位置的地址'));
-    return list;
+class TableDefinition {
+  TableDefinition({
+    this.name,
+    this.structure,
+    this.version = 1,
+  }) {
+    _init();
   }
 
-  static List<PersonItem> getPersonList() {
-    List<PersonItem> list = List<PersonItem>();
-    list.add(PersonItem(name: '咪娃娃'));
-    list.add(PersonItem(name: '贝贝黑'));
-    list.add(PersonItem(name: '雪嫘嫘'));
-    list.add(PersonItem(name: '小叮当'));
-    list.add(PersonItem(name: '小丸子'));
-    list.add(PersonItem(name: '章飞了'));
-    list.add(PersonItem(name: '花家家'));
-    list.add(PersonItem(name: '大老爷'));
-    return list;
+  String name;
+  String structure ;
+  int version;
+
+}
+
+class TimeDataBase {
+  TimeDataBase({
+    this.version = 1,
+    this.tableList,
+  }){
+    _init().then((databasesPath){path = join(databasesPath, "TimeMoment.db");});
   }
+
+  String path;
+  int version;
+  List<TableDefinition> tableList;
+  Database database;
+
+  Future _init() async {
+    return await getDatabasesPath();
+  }
+
+  Future openDataBase() async {
+    database = await openDatabase(path, version: version,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+            'CREATE TABLE ${_tableDefinition.name} (${_tableDefinition.structure})');
+      }
+    );
+  }
+
+  Future deleteDataBase() async {
+    await deleteDatabase(path);
+  }
+
+}
+
+class DateServices {
+  TableDefinition _definition = TableDefinition(
+    name: 'PersonTable',
+    structure: '''
+      boxId integer primary key autoincrement, 
+      name text not null,
+      age integer not null)
+    ''',
+  );
 
 }
